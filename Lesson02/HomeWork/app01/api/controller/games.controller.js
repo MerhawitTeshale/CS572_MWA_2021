e18cf13364adb44a1a8ce62cf1936319fa2e9bad
gmtma <43379424+gmtma@users.noreply.github.com>
2021-06-23 20:36:15 -0500

    Lesson02
    assignment and class works done today

---
+++ Lesson02/HomeWork/app01/api/controller/games.controller.js
@@ -0,0 +1,27 @@
+const { query } = require('express');
+const gamesData=require('../data/games.json');
+const dbConnection=require('../data/dbconnection');
+module.exports.gamesGetAll=(req,res)=>{
+    console.log(`this is the Json route`);
+    console.log(`${req.query}`);
+
+    let offset=0;
+    if(req.query&&!req.query.offset){
+        console.log(req.query.offset);
+        offset=4;
+    }
+    if(req.query&&req.query.offset){
+        if(parseInt(req.query.offset)<=8){
+        offset=parseInt(req.query.offset);
+    } else{
+        offset=8;
+    }
+    }
+    const db=dbConnection.get();
+    const collection=db.collection("games");
+    const docs=collection.find().limit(offset).toArray((err,docs)=>{
+        res.status(200).json(docs);
+    });
+    
+};
+
---
+++ Lesson02/HomeWork/app01/api/data/dbconnection.js
@@ -0,0 +1,25 @@
+const MongoClient=require('mongodb').MongoClient;
+const dbName= "meanGames";
+const dburl="mongodb://localhost:27017/"+dbName;
+
+let _connection=null;
+
+const open=()=>{
+    MongoClient.connect(dburl,{useUnifiedTopology:true},(err,client)=>{
+        if (err){
+            console.log(`Database connection error due to ${err}`);
+            return
+        }
+        _connection=client.db(dbName);
+        console.log(`connection open ${_connection} `);
+    });
+};
+
+let get=()=>{
+    return _connection;
+};
+
+module.exports={
+    open:open,
+    get:get
+};
